f8a40c3b89020db42d70cc29be7de26ffdcf8601
Hassan Ezz <dhassanezz98@gmail.com>
2024-01-23 13:11:11 +0200

    pulled

--- packages/server/src/routes/index.ts
+++ packages/server/src/routes/index.ts
@@ -1,6 +1,8 @@
 import { Hono } from 'hono'
 import { app as usersRoutes } from './users'
+import { app as boardsRoutes } from './boards'
 
 export const app = new Hono()
 
 app.route('/users', usersRoutes)
+app.route('/boards', boardsRoutes)
--- packages/server/src/routes/users/index.ts
+++ packages/server/src/routes/users/index.ts
@@ -1,7 +1,38 @@
 import { Hono } from 'hono'
+import { validator } from 'hono/validator'
+import { z } from 'zod'
+import { prisma } from '../../db'
 
 export const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('This is users')
+app.post('/', validator('json', (value, c) => {
+
+  // TODO add proper schema
+  const schema = z.object({
+    fullName: z.string(),
+    username: z.string(),
+    email: z.string(),
+    profilePictureURL: z.string()
+  })
+
+  const parsed = schema.safeParse(value)
+
+  if (!parsed.success)
+    return c.json(parsed.error, 401)
+
+  return parsed.data
+}), async (c) => {
+
+  const { fullName, username, email, profilePictureURL } = c.req.valid('json')
+
+  const newUser = await prisma.user.create({
+    data: {
+      fullName,
+      username,
+      email,
+      profilePictureURL
+    },
+  })
+
+  return c.json(newUser)
 })
