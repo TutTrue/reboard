f87cac608313df98091d3b70635e9ad9c0b75e5d
Hassan Ezz <dhassanezz98@gmail.com>
2024-01-31 20:18:36 +0200

    added routes, create board, get board details

--- packages/server/src/routes/boards/index.ts
+++ packages/server/src/routes/boards/index.ts
@@ -1,6 +1,8 @@
 import { Hono } from 'hono'
 import { prisma } from '../../db/index'
 import { type AuthVariables, authMiddleware } from '../../middleware'
+import { zValidator } from '@hono/zod-validator'
+import { z } from 'zod'
 
 export const app = new Hono<{ Variables: AuthVariables }>()
 
@@ -16,8 +18,8 @@ app.get('/', async (c) => {
           include: {
             Task: true,
             List: true,
-            UserBoards: true
-          }
+            UserBoards: true,
+          },
         },
       },
       where: {
@@ -30,3 +32,80 @@ app.get('/', async (c) => {
     return c.json({ message: 'Internal server error' }, 500)
   }
 })
+
+app.get('/:username/:boardName', async (c) => {
+  const { username, boardName } = c.req.param()
+
+  // check if user have access to the board or board exist
+  const board = await prisma.board.findFirst({
+    where: {
+      name: boardName,
+      UserBoards: {
+        some: {
+          username,
+        },
+      },
+    },
+    include: {
+      List: true,
+      Task: true,
+    },
+  })
+
+  if (!board) return c.json({ message: 'Board can not be found' }, 404)
+
+  return c.json(board)
+})
+
+app.post(
+  '/',
+  zValidator(
+    'json',
+    z.object({
+      name: z
+        .string()
+        .min(1)
+        .max(255)
+        .regex(/^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}$/i),
+    })
+  ),
+  async (c) => {
+    try {
+      const decodedJwtPayload = c.get('decodedJwtPayload')
+      const name = c.req.valid('json').name.toLowerCase()
+
+      const boardWithSameName = await prisma.board.findFirst({
+        where: {
+          name,
+          UserBoards: {
+            every: {
+              id: decodedJwtPayload?.id,
+            },
+          },
+        },
+      })
+
+      // make sure a user can't have two boards with the same name/slug
+      if (boardWithSameName)
+        return c.json(
+          { message: 'A board with the same name already exists' },
+          401
+        )
+
+      const newBoard = await prisma.board.create({
+        data: {
+          name,
+          UserBoards: {
+            connect: {
+              id: decodedJwtPayload?.id,
+            },
+          },
+        },
+      })
+
+      return c.json(newBoard)
+    } catch (e) {
+      return c.json({ message: 'Internal server error' }, 500)
+    }
+  }
+)
