869b1f067c585e1aee60c66e386099a7f2b7ac4f
Mahmoud Hamdy <hamdy.xd55@gmail.com>
2024-02-01 12:46:44 +0200

    [server] added usage of CORS

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,11 +1,13 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
 
 import { app as routes } from './routes'
 
 const app = new Hono()
 const PORT = (process.env.PORT || 3001) as number
 
+app.use('/api/*', cors())
 app.route('/api', routes)
 
 console.log(`Server is running on port ${PORT}`)
