f9fb2414f221b60ecbfad8a17c986d3aa6daa35a
Hassan Ezz <dhassanezz98@gmail.com>
2024-02-07 16:29:56 +0200

    [client] changed style of displaying the boards n the dashboard

--- packages/client/src/app/[username]/page.tsx
+++ packages/client/src/app/[username]/page.tsx
@@ -27,7 +27,7 @@ export default async function Dashboard({
         <CreateBoardModal />
       </div>
 
-      <div className="flex items-start flex-wrap gap-5">
+      <div className="grid grid-cols-2 gap-3">
         {boards?.map((board) => (
           <BoardCard key={board.id} board={board} session={session} />
         ))}
