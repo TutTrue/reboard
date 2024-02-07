4daca92a7b83d0b90e2c908f7b6231849d775299
Mahmoud Hamdy <hamdy.xd55@gmail.com>
2024-02-07 19:00:56 +0200

    [client] remove user from a board

--- packages/client/src/app/lib/serverActions.ts
+++ packages/client/src/app/lib/serverActions.ts
@@ -202,3 +202,25 @@ export async function updateTaskAction(
     return { success: false, error: e.response.data }
   }
 }
+
+export async function removeUserFromBoardAction(
+  boardId: string,
+  username: string
+): Promise<APIRespone<BoardWithRelations>> {
+  try {
+    const res = await fetcher.patch(
+      `/boards/${boardId}`,
+      { username },
+      {
+        headers: {
+          Authorization: await getToken(),
+        },
+      }
+    )
+    
+    revalidatePath('/TeamTab')
+    return { success: true, data: res.data }
+  } catch (e: any) {
+    return { success: false, error: e.response.data }
+  }
+}
--- packages/client/src/components/BoardView/TeamTab.tsx
+++ packages/client/src/components/BoardView/TeamTab.tsx
@@ -1,8 +1,10 @@
-import { BoardWithRelations } from '@/types'
 import { Session } from 'next-auth'
 import Image from 'next/image'
-import { Button } from '../ui/button'
+import toast from 'react-hot-toast'
 import { HiOutlineUserRemove } from 'react-icons/hi'
+import { Button } from '../ui/button'
+import { BoardWithRelations, IUser } from '@/types'
+import { removeUserFromBoardAction } from '@/app/lib/serverActions'
 
 interface TeamTabProps {
   authedUser: Session['user']
@@ -10,42 +12,61 @@ interface TeamTabProps {
 }
 
 export default function TeamTab({ board, authedUser }: TeamTabProps) {
+
+
+
   return (
     <div className="flex flex-col bg-white divide-y">
       {board.UserBoards.map((user) => (
-        <div
-          key={user.id}
-          className="flex items-center justify-between px-4 py-3"
-        >
-          <div className="flex items-center gap-3">
-            <Image
-              src={user.profilePictureURL || '/images/default-user.png'}
-              alt="user image"
-              width={45}
-              height={45}
-              className="rounded-full border-2 border-white"
-            />
-
-            <div className="flex flex-col">
-              <h3>
-                {user.fullName}
-                {board.Owner.username === user.username ? ' 👑' : ''}
-              </h3>
-              <p className="text-gray-500 text-sm">@{user.username}</p>
-            </div>
-          </div>
-
-          {authedUser.username === board.Owner.username &&
-          user.username !== board.Owner.username ? (
-            <Button variant={'destructive'} className="gap-2">
-              <HiOutlineUserRemove size={18} />
-              <span>Kick</span>
-            </Button>
-          ) : (
-            ''
-          )}
-        </div>
+        <Member key={user.id} user={user} board={board} authedUser={authedUser} />
       ))}
+    </div>
+  )
+}
+
+function Member({ user, board, authedUser }: { user: IUser, board: BoardWithRelations, authedUser: Session['user'] }) {
+
+  async function handleRemoveUser() {
+    const res = await removeUserFromBoardAction(board.id, user.username)
+
+    if (!res.success) {
+      toast.error('Error removing user')
+      return
+    }
+    toast.success('User removed successfully')
+  }
+
+  return (
+    <div
+      className="flex items-center justify-between px-4 py-3"
+    >
+      <div className="flex items-center gap-3">
+        <Image
+          src={user.profilePictureURL || '/images/default-user.png'}
+          alt="user image"
+          width={45}
+          height={45}
+          className="rounded-full border-2 border-white"
+        />
+
+        <div className="flex flex-col">
+          <h3>
+            {user.fullName}
+            {board.Owner.username === user.username ? ' 👑' : ''}
+          </h3>
+          <p className="text-gray-500 text-sm">@{user.username}</p>
+        </div>
+      </div>
+
+      {authedUser.username === board.Owner.username &&
+        user.username !== board.Owner.username ? (
+        <Button variant={'destructive'} className="gap-2" onClick={handleRemoveUser}>
+          <HiOutlineUserRemove size={18} />
+          <span>Kick</span>
+        </Button>
+      ) : (
+        ''
+      )}
     </div>
   )
 }
