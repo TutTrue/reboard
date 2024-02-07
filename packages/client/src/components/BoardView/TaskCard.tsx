1b3403a46d1d14f3958dd30c2b3d2309cc1bd9bf
Hassan Ezz <dhassanezz98@gmail.com>
2024-02-07 16:33:25 +0200

    [client] changed todo toggle styles

--- packages/client/src/components/BoardView/TaskCard.tsx
+++ packages/client/src/components/BoardView/TaskCard.tsx
@@ -13,6 +13,7 @@ import { TaskWithRelations } from '@/types'
 import Image from 'next/image'
 import toast from 'react-hot-toast'
 import { deleteTaskAction, updateTaskAction } from '@/app/lib/serverActions'
+import { HiOutlineCheck } from 'react-icons/hi'
 
 function TaskDropDownMenu({ task }: { task: TaskWithRelations }) {
   async function handleDelete() {
@@ -34,7 +35,10 @@ function TaskDropDownMenu({ task }: { task: TaskWithRelations }) {
           <span>Edit</span>
         </DropdownMenuItem>
 
-        <DropdownMenuItem className="flex items-center gap-2" onClick={handleDelete}>
+        <DropdownMenuItem
+          className="flex items-center gap-2"
+          onClick={handleDelete}
+        >
           <HiOutlineTrash size={18} />
           <span>Delete</span>
         </DropdownMenuItem>
@@ -44,14 +48,23 @@ function TaskDropDownMenu({ task }: { task: TaskWithRelations }) {
 }
 
 function TaskCard({ task }: { task: TaskWithRelations }) {
+
   async function handleToggleComplete() {
-    const res = await updateTaskAction(task.id, undefined, undefined, !task.completed)
+    const res = await updateTaskAction(
+      task.id,
+      undefined,
+      undefined,
+      !task.completed
+    )
+
     if (!res.success) {
       toast.error('Error updating task')
       return
     }
+
     toast.success('Task updated successfully')
   }
+  
   return (
     <div className="bg-white px-3 py-2 hover:bg-gray-100 rounded-md flex items-center justify-between gap-3">
       <div className="flex items-center gap-2">
@@ -62,8 +75,17 @@ function TaskCard({ task }: { task: TaskWithRelations }) {
           height={25}
           className="rounded-full border-2 border-white"
         />
-        <button className="block w-5 h-5 bg-gray-300 rounded text-sm" onClick={handleToggleComplete}><span>{task.completed ? "✔️" : ""}</span></button>
-        <span className={task.completed ? "line-through" : ""}>{task.text}</span>
+        <button
+          className={`w-5 h-5 rounded text-sm text-white flex items-center justify-center ${
+            task.completed ? 'bg-indigo-500' : 'bg-gray-300'
+          }`}
+          onClick={handleToggleComplete}
+        >
+          {task.completed ? <HiOutlineCheck size={18} /> : ''}
+        </button>
+        <span className={task.completed ? 'line-through' : ''}>
+          {task.text}
+        </span>
       </div>
 
       <TaskDropDownMenu task={task} />
