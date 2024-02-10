0b32858527081a0963fad708b037d8252e655af6
Mahmoud Hamdy <hamdy.xd55@gmail.com>
2024-02-10 20:48:57 +0200

    [client] change leave board dropdown style

--- packages/client/src/components/BoardOptionDropdownMenu.tsx
+++ packages/client/src/components/BoardOptionDropdownMenu.tsx
@@ -4,9 +4,8 @@ import {
   HiOutlineEllipsisHorizontal,
   HiOutlinePencil,
   HiOutlineTrash,
-  HiOutlineUserMinus
 } from 'react-icons/hi2'
-
+import { ImExit } from "react-icons/im";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -39,31 +38,31 @@ export default function BoardOptionDropdownMenu({
       <DropdownMenuTrigger>
         <HiOutlineEllipsisHorizontal size={27} />
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="text-gray-500">
-        {isOwner ? (
-          <>
-            <DropdownMenuItem
-              className="flex items-center gap-2"
-              onClick={handleEdit}
-            >
-              <HiOutlinePencil size={18} />
-              <span>Edit</span>
-            </DropdownMenuItem>
-            <DropdownMenuItem
-              className="flex items-center gap-2"
-              onClick={handleDelete}
-            >
-              <HiOutlineTrash size={18} />
-              <span>Delete</span>
-            </DropdownMenuItem>
-          </>
-        ) : (
-          <DropdownMenuItem className="flex items-center gap-2">
-            <HiOutlineUserMinus size={18} />
+      {isOwner ? (
+        <DropdownMenuContent className="text-gray-500">
+          <DropdownMenuItem
+            className="flex items-center gap-2"
+            onClick={handleEdit}
+          >
+            <HiOutlinePencil size={18} />
+            <span>Edit</span>
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            className="flex items-center gap-2"
+            onClick={handleDelete}
+          >
+            <HiOutlineTrash size={18} />
+            <span>Delete</span>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      ) : (
+        <DropdownMenuContent className="text-white bg-red-600 hover:bg-red-400">
+          <DropdownMenuItem className="flex items-center gap-2 hover focus:bg-red-400 focus:text-white">
+            <ImExit size={18} />
             <span>Leave board</span>
           </DropdownMenuItem>
-        )}
-      </DropdownMenuContent>
+        </DropdownMenuContent>
+      )}
     </DropdownMenu>
   )
 }
