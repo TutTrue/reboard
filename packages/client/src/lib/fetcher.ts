ab7a6337307c98f96ca3ffb0fef8ac65377e3c2a
Mahmoud Hamdy Abdelhady <106309885+TutTrue@users.noreply.github.com>
2024-02-14 10:45:01 +0200

    [client] adjust next auth cookie with https

--- packages/client/src/lib/fetcher.ts
+++ packages/client/src/lib/fetcher.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 import { cookies } from 'next/headers'
 
 export async function getToken() {
-  return cookies().get('next-auth.session-token')?.value || ''
+  return cookies().get('next-auth.session-token')?.value || cookies().get('__Secure-next-auth.session-token')?.value || ''
 }
 
 export const fetcher = axios.create({
